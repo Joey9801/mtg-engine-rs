7f7a8801b39021b52b542fc6f1e6d38f215d7c6a
Joe Roberts <josephroberts11@gmail.com>
2021-03-07 21:55:54 +0000

    Simple debug example in main.rs

---
+++ core/src/main.rs
@@ -0,0 +1,50 @@
+use mtg_engine_core::{BaseObserver, Controller, actions::{Action, BaseAction}, base_rules, game::{GameBuilder, Game}};
+
+#[derive(Clone, Debug)]
+struct StdoutDebugObserver {}
+
+impl BaseObserver for StdoutDebugObserver {
+    fn observe_action(&mut self, action: &Action, game: &Game, emit_action: &mut dyn FnMut(BaseAction)) {
+        dbg!(action);
+    }
+}
+
+fn main() {
+    let mut game = GameBuilder::new()
+        .with_player("alice")
+        .with_player("bob")
+        .with_starting_life_total(20)
+        .with_implicit_turn_order()
+        .build();
+
+    base_rules::attach(&mut game);
+    game.attach_observer(Box::new(StdoutDebugObserver {}));
+
+    dbg!(&game);
+
+    let alice = game.find_player("alice").unwrap();
+    let bob = game.find_player("bob").unwrap();
+    let fake_oid = game.observer_id_gen.next_id();
+
+    game.staging_actions.push(Action {
+        base_action: BaseAction::SetPriority(alice),
+        controller: Controller::Game,
+        source: fake_oid,
+        original: None,
+    });
+    game.tick_until_player_input();
+    game.staging_actions.push(Action {
+        base_action: BaseAction::PassPriority,
+        controller: Controller::Player(alice),
+        source: fake_oid,
+        original: None,
+    });
+    game.tick_until_player_input();
+    game.staging_actions.push(Action {
+        base_action: BaseAction::SetPriority(bob),
+        controller: Controller::Game,
+        source: fake_oid,
+        original: None,
+    });
+    game.tick_until_player_input();
+}
